a9205caca946af396621e9e13bfb09a7bd32ebc5
Ssssssaber <bladimir.lobanov@gmail.com>
2026-03-08 16:45:27 +0700

    (fix): pivot positions

--- Assets/Scripts/Character/HealthManipulation/AttackHitbox.cs
+++ Assets/Scripts/Character/HealthManipulation/AttackHitbox.cs
@@ -5,6 +5,25 @@ namespace Health
 {
     public class AttackHitbox : MonoBehaviour
     {
-        
+        [SerializeField] private uint _damage = 1;
+        [SerializeField] private DamageDealer _hitboxCollider;
+
+        private void Awake()
+        {
+            _hitboxCollider.enabled = false;
+            _hitboxCollider.SetDamage(_damage);
+        }
+
+        // Called by Animation Event
+        public void EnableHitbox()
+        {
+            _hitboxCollider.SetEnabled(true);
+        }
+
+        // Called by Animation Event
+        public void DisableHitbox()
+        {
+            _hitboxCollider.SetEnabled(false);
+        }
     }
 }
--- Assets/Scripts/Character/HealthManipulation/DamageDealer.cs
+++ Assets/Scripts/Character/HealthManipulation/DamageDealer.cs
@@ -3,9 +3,27 @@ using UnityEngine;
 
 namespace Health
 {
+    [RequireComponent(typeof(Collider2D))]
     public class DamageDealer : MonoBehaviour
     {
         [SerializeField] private uint _damageAmount = 1;
+        private Collider2D _attachedCollider;
+
+        private void Awake()
+        {
+            _attachedCollider = GetComponent<Collider2D>();
+            _attachedCollider.isTrigger = true;
+        }
+
+        public void SetDamage(uint damage)
+        {
+            _damageAmount = damage;
+        }
+
+        public void SetEnabled(bool enabled)
+        {
+            _attachedCollider.enabled = enabled;
+        }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
