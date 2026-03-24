b72984fd454e7c57c1bbd082412f9123b9571995
Ssssssaber <bladimir.lobanov@gmail.com>
2026-03-24 11:11:10 +0700

    (update): raycast for npc trigger zones to check if player is visible

--- Assets/Scripts/Character/StatePattern/NPC/NPCTriggerProxy.cs
+++ Assets/Scripts/Character/StatePattern/NPC/NPCTriggerProxy.cs
@@ -6,6 +6,16 @@ namespace NPC
     public class NPCTriggerProxy : MonoBehaviour
     {
         public NPCTriggerZoneType ZoneType;
+        
+        [Header("Vision Settings")]
+        [Tooltip("If true, will also check raycast to verify player is visible")]
+        [SerializeField] private bool _dependentOnVision = true;
+        
+        [Tooltip("Layers that block vision (e.g. walls)")]
+        [SerializeField] private LayerMask _visionBlockingLayers = Physics2D.DefaultRaycastLayers;
+
+        private int _ownColliderLayer;
+
         private NPCStateMachine _parentAI;
         private CircleCollider2D _collider;
 
@@ -13,24 +23,71 @@ namespace NPC
         {
             _parentAI = GetComponentInParent<NPCStateMachine>();
             _collider = GetComponent<CircleCollider2D>();
+            _ownColliderLayer = _collider.gameObject.layer;
+
+            if (_dependentOnVision && _visionBlockingLayers == 0)
+            {
+                _visionBlockingLayers = LayerMask.GetMask("Default");
+            }
         }
 
         public bool IsPlayerIn()
         {
+            if (GameManager.Instance?.Player == null) return false;
+
             Vector2 playerPos = GameManager.Instance.Player.transform.position;
-            return _collider.OverlapPoint(playerPos);
+            bool inTrigger = _collider.OverlapPoint(playerPos);
+
+            if (_dependentOnVision && inTrigger)
+            {
+                return IsPlayerVisible();
+            }
+
+            return inTrigger;
+        }
+
+        private bool IsPlayerVisible()
+        {
+            if (GameManager.Instance?.Player == null) return false;
+
+            GameObject player = GameManager.Instance.Player.gameObject;
+            Vector2 from = transform.position;
+            Vector2 to = player.transform.position;
+
+            int layerMask = _visionBlockingLayers & ~(1 << _ownColliderLayer);
+
+            RaycastHit2D hit = Physics2D.Linecast(from, to, layerMask);
+
+            if (hit.collider == null)
+            {
+                return true;
+            }
+
+            return hit.collider.gameObject == player;
         }
 
         private void OnTriggerEnter2D(Collider2D other)
         {
+            if (!ShouldNotify(other)) return;
+            
             Debug.Log($"{ZoneType} entered by {other.gameObject.name}");
             _parentAI.NotifyZoneEnter(ZoneType, other);
         }
 
         private void OnTriggerExit2D(Collider2D other)
         {
+            if (!ShouldNotify(other)) return;
+            
             Debug.Log($"{ZoneType} exited by {other.gameObject.name}");
             _parentAI.NotifyZoneExit(ZoneType, other);
+        }
+
+        private bool ShouldNotify(Collider2D other)
+        {
+            if (GameManager.Instance?.Player == null) return true;
+            if (other.gameObject != GameManager.Instance.Player.gameObject) return false;
+            if (_dependentOnVision) return IsPlayerVisible();
+            return true;
         }
     }
 }
